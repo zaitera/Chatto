9b30fcaf7da60258238b31bc6afb9148d780e460
zaitera <abdu.zaiter@gmail.com>
2024-09-24 09:20:37 +0200

    Implement screens in the compose app

--- composeApp/src/commonMain/kotlin/dev/zaitech/chatto/App.kt
+++ composeApp/src/commonMain/kotlin/dev/zaitech/chatto/App.kt
@@ -1,42 +1,76 @@
 package dev.zaitech.chatto
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import chatto.composeapp.generated.resources.Res
-import chatto.composeapp.generated.resources.compose_multiplatform
-import io.getstream.chat.android.models.User
+import io.getstream.chat.android.compose.ui.channels.ChannelsScreen
+import io.getstream.chat.android.compose.ui.theme.ChatTheme
+import io.getstream.chat.android.models.InitializationState
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import chatto.composeapp.generated.resources.app_name
+import io.getstream.chat.android.compose.ui.channels.SearchMode
+import io.getstream.chat.android.compose.ui.messages.MessagesScreen
+import io.getstream.chat.android.compose.viewmodel.messages.MessagesViewModelFactory
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        val chatClient by remember { StreamChat.getChatClient() }
-        val user = User("compose-user")
-        chatClient.connectUser(user = user, token = "token")
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+    val chatClient = rememberChatClient()
+    val clientInitializationState by rememberClientInitializationState(chatClient)
+    val navController = rememberNavController()
+    ChatTheme {
+        NavHost(navController = navController, startDestination = "channels") {
+            composable("channels") {
+                when (clientInitializationState) {
+                    InitializationState.COMPLETE -> {
+                        ChannelsScreen(
+                            title = stringResource(Res.string.app_name),
+                            isShowingHeader = true,
+                            searchMode = SearchMode.Channels,
+                            onChannelClick = { channel ->
+                                navController.navigate("messages/${channel.type}:${channel.id}")
+                            },
+                            onBackPressed = {
+                                navController.popBackStack()
+                            },
+                        )
+                    }
+
+                    InitializationState.INITIALIZING -> {
+                        Text(text = "Initializing...")
+                    }
+
+                    InitializationState.NOT_INITIALIZED -> {
+                        Text(text = "Not initialized...")
+                    }
+                }
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                    Text("StreamChat: ${chatClient.clientState.isOffline}")
+            composable(
+                "messages/{channelId}",
+                arguments = listOf(navArgument("channelId") { type = NavType.StringType })
+            ) { backStackEntry ->
+                val channelId = backStackEntry.arguments?.getString("channelId")
+                if (channelId != null) {
+                    MessagesScreen(
+                        viewModelFactory = MessagesViewModelFactory(
+                            context = LocalContext.current,
+                            channelId = channelId
+                        ),
+                        onBackPressed = {
+                            navController.popBackStack()
+                        }
+                    )
                 }
             }
         }
     }
 }
+
