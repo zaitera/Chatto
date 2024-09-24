4d4a5c406dc6880b2df986f2713c422c8e010e32
zaitera <abdu.zaiter@gmail.com>
2024-09-24 09:20:02 +0200

    Create stream properties with remember allowing the handling with recomposition

--- composeApp/src/commonMain/kotlin/dev/zaitech/chatto/StreamChat.kt
+++ composeApp/src/commonMain/kotlin/dev/zaitech/chatto/StreamChat.kt
@@ -1,27 +1,63 @@
 package dev.zaitech.chatto
 
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.client.logger.ChatLogLevel
+import io.getstream.chat.android.models.InitializationState
+import io.getstream.chat.android.models.User
 import io.getstream.chat.android.offline.plugin.factory.StreamOfflinePluginFactory
 import io.getstream.chat.android.state.plugin.config.StatePluginConfig
 import io.getstream.chat.android.state.plugin.factory.StreamStatePluginFactory
-import kotlin.reflect.KProperty
 
-object StreamChat {
-    fun getChatClient(): ChatClient {
-        // Set up the OfflinePlugin for offline storage
-        val appContext = getAppContext()
-        val offlinePluginFactory = StreamOfflinePluginFactory(appContext = appContext)
-        val statePluginFactory = StreamStatePluginFactory(config = StatePluginConfig(), appContext = appContext)
-        // Set up the client for API calls and with the plugin for offline storage
-        val client = ChatClient.Builder("uun7ywwamhs9", appContext)
+@Composable
+fun rememberClientInitializationState(client: ChatClient): State<InitializationState> {
+    return client.clientState.initializationState.collectAsState()
+}
+
+@Composable
+fun rememberChatClient(): ChatClient {
+    val context = LocalContext.current
+
+    // Initialize plugins
+    val offlinePluginFactory = remember {
+        StreamOfflinePluginFactory(appContext = context)
+    }
+    val statePluginFactory = remember {
+        StreamStatePluginFactory(StatePluginConfig(), appContext = context)
+    }
+
+    // Build the ChatClient
+    val client = remember {
+        ChatClient.Builder(Secrets.STREAM_API_KEY, context)
             .withPlugins(offlinePluginFactory, statePluginFactory)
             .logLevel(ChatLogLevel.ALL) // Set to NOTHING in prod
             .build()
-        return client
     }
-}
 
-operator fun ChatClient.getValue(thisObj: Any?, property: KProperty<*>): ChatClient {
-    return this
+    // Connect the user
+    LaunchedEffect(client) {
+        val user = User(
+            id = "leandro",
+            name = "Leandro Borges Ferreira",
+            image = "https://ca.slack-edge.com/T02RM6X6B-U01AQ67NJ9Z-2f28d711cae9-128",
+        )
+        client.connectUser(
+            user = user,
+            token = Secrets.STREAM_CLIENT_TOKEN,
+        ).enqueue{ result ->
+            if (result.isSuccess) {
+                Log.d("ChatClient", "User connected successfully")
+            } else {
+                Log.e("ChatClient", "Error connecting user: $result")
+            }
+        }
+    }
+
+    return client
 }
